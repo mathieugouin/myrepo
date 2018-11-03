7387727e606f6fe2a5c233f5f96201415726248d
Mathieu Gouin <mgouin@gmail.com>
2018-11-02 22:47:55 -0400

    int ch: asset1

--- integration.c
+++ integration.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "asset-1.h"
+
 static int i1 = 1;
 static int i2 = 2;
 
@@ -6,4 +8,6 @@ void integration(void)
 {
   int i = i1 + i2;
   printf("Hello from integration() %d\n", i);
+
+  asset1();
 }
