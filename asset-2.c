8178f5b4401a954cb033e9fbcb167ea99d061d33
Mathieu Gouin <mgouin@gmail.com>
2018-11-02 22:29:31 -0400

    a2 ch

--- asset-2.c
+++ asset-2.c
@@ -1,6 +1,12 @@
 #include "asset-2.h"
 
-static int asset2 = 2;
-static int asset2a = 2;
-static int asset2b = 2;
+static int a2 = 2;
+static int a2a = 2;
+static int a2b = 2;
 
+
+void asset2(void)
+{
+  int a = a2 + a2a + a2b;
+  (void)a;
+}
