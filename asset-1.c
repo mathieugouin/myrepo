94283073317bf591a9deccbb62280ee606336d80
Mathieu Gouin <mgouin@gmail.com>
2018-11-02 22:25:32 -0400

    a1 ch

--- asset-1.c
+++ asset-1.c
@@ -1,9 +1,10 @@
 #include "asset-1.h"
 
-static int asset1 = 1; // test
-static int asset1a = 1; // test
+static int a1 = 1; // test
+static int a1a = 1; // test
 
 void asset1(void)
 {
-
+  int a = a1 + a1a;
+  (void)a;
 }
