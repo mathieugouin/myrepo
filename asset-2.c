1c7b5b7e60323e5980073c4a6acfe4c307bfc173
Mathieu Gouin <mgouin@gmail.com>
2018-11-02 22:40:04 -0400

    a2 ch

--- asset-2.c
+++ asset-2.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "asset-2.h"
 
 static int a2 = 2;
@@ -7,5 +8,5 @@ static int a2b = 2;
 void asset2(void)
 {
   int a = a2 + a2a + a2b;
-  (void)a;
+  printf("Hello from asset2() %d\n", a);
 }
